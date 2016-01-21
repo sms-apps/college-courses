df30be09152b99b4980e9a694465dac7d651fa1a
Scott Smith <scott@sms-apps.com>
2016-01-21 15:36:26 -0700

    GREEN for DetermineClassOrderTest

--- CollegeCourses/CollegeCourses.Business/Engine.cs
+++ CollegeCourses/CollegeCourses.Business/Engine.cs
@@ -1,13 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace CollegeCourses.Business
 {
     public class Engine : IEngine
     {
+        private readonly IMapper _mapper;
+
+        public Engine(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
         public IEnumerable<string> DetermineClassOrder(IEnumerable<string> classes)
         {
-            throw new NotImplementedException();
+            var result = new List<string>();
+
+            var courses = _mapper.ToCourses(classes);
+
+            return result;
         }
     }
 }
--- CollegeCourses/CollegeCourses.Tests/EngineTests.cs
+++ CollegeCourses/CollegeCourses.Tests/EngineTests.cs
@@ -1,7 +1,7 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using CollegeCourses.Business;
-using System.Collections.Generic;
+﻿using CollegeCourses.Business;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Ninject;
+using System.Collections.Generic;
 
 namespace CollegeCourses.Tests
 {
@@ -9,13 +9,15 @@ namespace CollegeCourses.Tests
     public class EngineTests
     {
         private static IEngine _collegeCoursesEngine;
+        private static IMapper _mapper;
         private static StandardKernel _kernel;
 
         [ClassInitialize]
         public static void TestInitialize(TestContext testContext)
         {
             _kernel = new StandardKernel(new CollegeCoursesNinjectModule());
-            _collegeCoursesEngine = new Engine();
+            _mapper = new Mapper();
+            _collegeCoursesEngine = new Engine(_mapper);
         }
 
         [TestMethod]
@@ -28,8 +30,8 @@ namespace CollegeCourses.Tests
         [TestMethod]
         public void DetermineClassOrderTest()
         {
-            _collegeCoursesEngine.DetermineClassOrder(new List<string>());
+            var result = _collegeCoursesEngine.DetermineClassOrder(new List<string>());
+            Assert.IsNotNull(result);
         }
-
     }
 }
