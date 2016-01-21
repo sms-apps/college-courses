3b5917903a840506eaf26257cfdf433993e793a5
Scott Smith <scott@sms-apps.com>
2016-01-21 14:12:28 -0700

    General cleanup

--- CollegeCourses/CollegeCourses.Tests/EngineTests.cs
+++ CollegeCourses/CollegeCourses.Tests/EngineTests.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using CollegeCourses.Business;
 using System.Collections.Generic;
 
