17d7700ebbacb238413cfcd5c08db6228abce68b
Scott Smith <scott@sms-apps.com>
2016-01-21 15:45:06 -0700

    GREEN for DetermineClassOrderTest

--- CollegeCourses/CollegeCourses.Business/Engine.cs
+++ CollegeCourses/CollegeCourses.Business/Engine.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace CollegeCourses.Business
 {
@@ -16,6 +17,10 @@ namespace CollegeCourses.Business
             var result = new List<string>();
 
             var courses = _mapper.ToCourses(classes);
+            result.AddRange(courses
+                .Where(c => string.IsNullOrWhiteSpace(c.PrerequisiteTitle))
+                .Select(f => f.Title)
+            );
 
             return result;
         }
--- CollegeCourses/CollegeCourses.Tests/EngineTests.cs
+++ CollegeCourses/CollegeCourses.Tests/EngineTests.cs
@@ -34,7 +34,21 @@ namespace CollegeCourses.Tests
             var result = _collegeCoursesEngine.DetermineClassOrder(new List<string>());
             Assert.IsNotNull(result);
 
+            List<string> validInput = new List<string>
+            {
+                "Introduction to Paper Airplanes:",
+                "Advanced Throwing Techniques: Introduction to Paper Airplanes",
+                "History of Cubicle Siege Engines: Rubber Band Catapults 101",
+                "Advanced Office Warfare: History of Cubicle Siege Engines",
+                "Rubber Band Catapults 101:",
+                "Paper Jet Engines: Introduction to Paper Airplanes"
+            };
+            result = _collegeCoursesEngine.DetermineClassOrder(validInput);
             Assert.IsTrue(result.Any());
+
+            //var joinedResult = string.Join(", ", result);
+            //var expectedResult = "Introduction to Paper Airplanes, Rubber Band Catapults 101, Paper Jet Engines, Advanced Throwing Techniques, History of Cubicle Siege Engines, Advanced Office Warfare";
+            //Assert.AreEqual(joinedResult, expectedResult);
         }
     }
 }
